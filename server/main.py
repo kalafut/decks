14569a11b89901d4d8a70df3ae03f5ff766fa6f6
Jim Kalafut <jim@kalafut.net>
2016-05-07 15:13:11 -0700

    Allow db.py to be configured

--- server/main.py
+++ server/main.py
@@ -1,6 +1,19 @@
+import sys
+
+import db
 import server
 import config
 
+
 if __name__ == "__main__":
+    init = False
+    sample_data = False
+
+    if "init" in sys.argv:
+        init = True
+        sample_data = True
+
+    db.connect("sqlite:///test.db", init=init, sample_data=sample_data)
+
     print("Starting server on port 8888...")
     server.server_start(config)
